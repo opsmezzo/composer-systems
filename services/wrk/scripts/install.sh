c8e416014e1f75207ce94e8389dc1207bdcac1b9
Jarrett Cruger <jcruger@qrhook.com>
2013-04-02 01:22:10 -0400

    [fix] use better branch of wrk

--- services/wrk/scripts/install.sh
+++ services/wrk/scripts/install.sh
@@ -3,7 +3,7 @@ set -v
 
 pkgin -y install scmgit
 pushd /opt
-  git clone https://github.com/isaacs/wrk.git
+  git clone git://github.com/bnoordhuis/wrk.git
   cd wrk
   make
   ln wrk /opt/local/bin
