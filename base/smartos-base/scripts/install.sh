39f8b6868ae6d074fff77656fc3a0b5e40089c1e
Maciej Małecki <me@mmalecki.com>
2013-05-09 03:44:07 +0200

    [system] Do not install unncessary stuff in base system

--- base/smartos-base/scripts/install.sh
+++ base/smartos-base/scripts/install.sh
@@ -1,7 +1,7 @@
 #
 # Update the system
 #
-pkgin -y install scmgit gcc-compiler gmake openssl python27
+pkgin -y install scmgit
 
 cp ../files/limits $HOME/.limits
 echo '. .limits' >> $HOME/.bashrc
