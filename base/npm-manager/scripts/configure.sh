c49b94b38fad5754f37fa5d01b0828462997c5e8
Maciej Małecki <me@mmalecki.com>
2013-06-03 01:31:43 -0400

    [fix] Fix Bash syntax
    Double brackets are required when using `&&` or `||`.

--- base/npm-manager/scripts/configure.sh
+++ base/npm-manager/scripts/configure.sh
@@ -9,7 +9,7 @@ cp ../templates/npmrc $HOME/.npmrc
 # Load `quill_npm_*` environment variables into
 # $HOME/.npmrc if they exist.
 #
-if [ ! -z $q_npm_username && ! -z $q_npm_password ]; then
+if [[ ! -z $q_npm_username && ! -z $q_npm_password ]]; then
   AUTH=$(printf '%b:%b' $q_npm_username $q_npm_password | base64)
   echo "_auth = $AUTH" >> $HOME/.npmrc
 fi
