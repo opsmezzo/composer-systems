072a64c8033844fc990c0acbcfcae88d1f72f6d2
Maciej Małecki <me@mmalecki.com>
2013-07-28 16:26:56 +0200

    [system] Upgrade ubuntu servers before installing software

--- base/ubuntu-base/scripts/install.sh
+++ base/ubuntu-base/scripts/install.sh
@@ -2,5 +2,5 @@
 # Update the system
 #
 apt-get -y update 
-apt-get -y -f --force-yes install ntp
-apt-get -y install git-core build-essential libssl-dev pkg-config libbz2-dev
+apt-get -y upgrade
+apt-get -y install git-core build-essential libssl-dev pkg-config libbz2-dev ntp
