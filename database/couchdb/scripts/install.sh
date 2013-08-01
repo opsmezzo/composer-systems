58034d020800e13a119fe3f07493db82dda46442
Maciej Małecki <me@mmalecki.com>
2013-08-01 18:42:56 +0200

    [refactor] Upgrade CouchDB, install on Ubuntu only

--- database/couchdb/scripts/install.sh
+++ database/couchdb/scripts/install.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
-[ `uname` == 'Linux' ] && apt-get -y install erlang-dev erlang-manpages erlang-base-hipe erlang-eunit erlang-nox erlang-xmerl erlang-inets libmozjs185-dev libicu-dev libcurl4-gnutls-dev libtool make gcc build-essential
-[ `uname` == 'SunOS' ] && ((yes | pkgin update ) || ( yes | pkgin install scmgit gcc-compiler gmake openssl python27 ruby18-rake-0.8.7nb2 gmake-3.82nb2 zlib-1.2.3 ))
-
-cp ../files/https.pem $HOME/https.pem
+apt-get -y install gcc make libicu48 libicu-dev erlang libmozjs185-dev g++ logrotate
 
 ORIGINAL_DIR=`pwd`
 cd $HOME
-  wget http://www.us.apache.org/dist/couchdb/source/1.2.2/apache-couchdb-1.2.2.tar.gz
-  tar xvf apache-couchdb-1.2.2.tar.gz
-  cd apache-couchdb-1.2.2
+
+  curl http://www.us.apache.org/dist/couchdb/source/1.3.1/apache-couchdb-1.3.1.tar.gz | tar -zxvf-
+  cd apache-couchdb-1.3.1
   ./configure
   make
   make install
 
-  useradd -d /var/lib/couchdb couchdb
-  chown -R couchdb: /usr/local/var/{lib,log,run}/couchdb /usr/local/etc/couchdb
+  groupadd couchdb
+  useradd -d /var/lib/couchdb -g couchdb -s /bin/false couchdb
+  chown -R couchdb:couchdb /usr/local/var/{lib,log,run}/couchdb /usr/local/etc/couchdb
   chmod 0770 /usr/local/var/{lib,log,run}/couchdb/
   chmod 664 /usr/local/etc/couchdb/*.ini
   chmod 775 /usr/local/etc/couchdb/*.d
@@ -22,4 +20,5 @@ cd $HOME
   ln -s /usr/local/etc/logrotate.d/couchdb /etc/logrotate.d/couchdb
   ln -s /usr/local/etc/init.d/couchdb /etc/init.d/couchdb
   update-rc.d couchdb defaults
+
 cd $ORIGINAL_DIR
