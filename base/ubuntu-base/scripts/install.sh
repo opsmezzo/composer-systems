27bbf3d889e6dfd9912225bb921bf08d93f43298
Maciej Małecki <me@mmalecki.com>
2013-08-08 00:35:46 +0200

    [fix] Assume non-interactive frontend

--- base/ubuntu-base/scripts/install.sh
+++ base/ubuntu-base/scripts/install.sh
@@ -1,6 +1,7 @@
 #
 # Update the system
 #
+export DEBIAN_FRONTEND=noninteractive
 apt-get -y update 
 apt-get -y upgrade
 apt-get -y install git-core build-essential libssl-dev pkg-config libbz2-dev ntp
