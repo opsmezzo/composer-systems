38e78920eeab8aa90c37d7226e5d8ba92301a412
Maciej Małecki <me@mmalecki.com>
2013-08-13 02:19:21 +0200

    [fix] Add the whole buildchain

--- base/smartos-base/scripts/install.sh
+++ base/smartos-base/scripts/install.sh
@@ -1,7 +1,7 @@
 #
 # Update the system
 #
-pkgin -y install scmgit python27
+pkgin -y install scmgit python27 gcc-compiler gmake openssl
 
 cp ../files/limits $HOME/.limits
 grep '. .limits' $HOME/.bashrc || echo '. .limits' >> $HOME/.bashrc
