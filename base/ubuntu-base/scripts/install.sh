0fe113efbe16fb0491f127eaa2e2379739078824
Maciej Małecki <me@mmalecki.com>
2013-09-27 18:32:38 +0200

    [fix] Prevent grub from being upgraded
    Fix dialogs popping up even with DEBIAN_FRONTEND=noninteractive.

--- base/ubuntu-base/scripts/install.sh
+++ base/ubuntu-base/scripts/install.sh
@@ -2,6 +2,7 @@
 # Update the system
 #
 export DEBIAN_FRONTEND=noninteractive
+apt-mark hold grub grub2 grub-pc
 apt-get -y update 
 apt-get -y upgrade
 apt-get -y install git-core build-essential libssl-dev pkg-config libbz2-dev ntp
